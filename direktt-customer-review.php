4158d5f09e8c2edc7f3e52614faf12ed86132d07
rmackovic <rmackovic@gmail.com>
2025-10-11 21:31:10 +0200

    Direktt slug change

--- direktt-customer-review.php
+++ direktt-customer-review.php
@@ -21,7 +21,7 @@ function direktt_customer_review_activation_check() {
         require_once ABSPATH . 'wp-admin/includes/plugin.php';
     }
 
-    $required_plugin = 'direktt-plugin/direktt.php';
+    $required_plugin = 'direktt/direktt.php';
 
     if ( ! is_plugin_active( $required_plugin ) ) {
         add_action(
